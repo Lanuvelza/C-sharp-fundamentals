e4356d26fe05faf49d2c25538228faad891e8c82
Ian <ianlam1221@hotmail.com>
2021-07-30 03:10:56 -0700

    using branches and loops, find the sum of all integers 1 through 20 that are divisible by 3.

---
+++ Branches_and_Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Branches_and_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the sum of all integers 1 through 20 that are divisible by 3. 
+            int sum = 0; 
+            for (int i = 1; i <= 20; i++) {
+                if (i % 3 == 0) {
+                    sum += i; 
+                }
+            }
+
+            Console.WriteLine($"The sum is {sum}.");
+        }
+    }
+}
