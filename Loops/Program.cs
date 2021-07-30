3a6478ff8bd6f8e8132bc61c1b063f617671e936
Ian <ianlam1221@hotmail.com>
2021-07-30 03:01:59 -0700

    for loop, while loop, do-while loop

---
+++ Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while looping 
+            int counter = 0;
+            while (counter < 10) {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+
+            // do-while looping
+            counter = 0;
+            do {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+
+            // for looping
+            for (int index = 0; index < 10; index++) {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+        }
+    }
+}
