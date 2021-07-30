db060462764bf3bd5f57a391a4f2d31e197fbb8c
Ian <ianlam1221@hotmail.com>
2021-07-30 03:27:01 -0700

    list, arrays and collections

---
+++ ArraysListCollections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysListCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List example
+            // var refers to a variable of arbitrary type. 
+            var names = new List<string> { "<name>", "Ana", "Felipe" }; 
+            foreach (string name in names) {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            // We can also write it like this.
+            List<string> names2 = new List<string> { "<name>", "Ana", "Felipe" }; 
+            foreach (string name in names2) {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            // also acheives the same desired output
+            for (int i = 0; i < names.Count; i++) {
+                Console.WriteLine($"Hello {names[i].ToUpper()}!");
+            }
+            
+            // Adding and removing items in a list
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("<name>");
+
+            foreach (var name in names) {
+                Console.WriteLine(name);
+            }
+
+            // accessing items of the List
+            Console.WriteLine(names[1]); //Felipe
+            Console.WriteLine(names[0]); //Ana
+        }
+    }
+}
