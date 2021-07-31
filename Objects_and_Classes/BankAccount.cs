637d2126b36b19cf742bf1d415a5198c99350c26
Ian <ianlam1221@hotmail.com>
2021-07-31 16:50:05 -0700

    Introduction to objects and classes

---
+++ Objects_and_Classes/BankAccount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// bank account supports the following behaviour: 
+
+// It has a 10-digit number that uniquely identifies the bank account.
+// It has a string that stores the name or names of the owners.
+// The balance can be retrieved.
+// It accepts deposits.
+// It accepts withdrawals.
+// The initial balance must be positive.
+// Withdrawals cannot result in a negative balance.
+
+namespace Objects_and_Classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0; 
+                foreach(var item in allTransactions) {
+                    balance += item.Amount; 
+                }
+
+                return balance;
+            } 
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>(); 
+
+        public BankAccount(string name, decimal initalBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initalBalance, DateTime.Now, "Initial Balance");
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var desposit = new Transaction(amount, date, note);
+            allTransactions.Add(desposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            if (Balance - amount < 0) {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal); 
+        }
+
+        public string GetAccountHistory() 
+        {
+            var report = new StringBuilder(); 
+
+            // Header
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                //rows
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}"); 
+            }
+            return report.ToString();
+        }
+    }
+}
+
---
+++ Objects_and_Classes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// Object oriented programming for a bank account
+// bank account supports the following behaviour: 
+
+// It has a 10-digit number that uniquely identifies the bank account.
+// It has a string that stores the name or names of the owners.
+// The balance can be retrieved.
+// It accepts deposits.
+// It accepts withdrawals.
+// The initial balance must be positive.
+// Withdrawals cannot result in a negative balance.
+
+namespace Objects_and_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Bob", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine($"Account: {account.Balance}");
+
+            // test that the initial balances must be positive. 
+            try 
+            {
+                var invalidAccount =  new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e) 
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            // catch exceptions do not stop the program from entirely running the
+            // this line below will still run as proceeded unless there is a return statement inside of the catch block
+            account.MakeWithdrawal(20, DateTime.Now, "Phasmophobia");
+            Console.WriteLine($"Account: {account.Balance}");
+
+            //test for a negative balance. 
+            try 
+            {
+                account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            } 
+
+            Console.WriteLine(account.GetAccountHistory());
+
+        }
+    }
+}
